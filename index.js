32a541e417c77d37caf9a3e4015a962e5d43281f
davideliason <davethemaker@gmail.com>
2017-07-12 08:39:41 -0700

    create reducer that simply returns initial state at this point

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const initialState = {
 	VisibilityFilter: VisibilityFilters.SHOW_ALL,
 	todos: []
 }
+
+function todoApp(state,action){
+	if(typeof(state)==='undefined'){
+		return initialState
+	}
+
+	return state
+}
