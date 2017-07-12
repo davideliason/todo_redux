f20a80dcd109dcd19c50703adb47f4d44eb86f97
davideliason <davethemaker@gmail.com>
2017-07-12 09:11:53 -0700

    add reducer function for adding a todo item

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { VisibilityFilters} from './actions';
+import { VisibilityFilters, ADD_TODO, TOGGLE_TODO } from './actions';
 
 const initialState = {
 	VisibilityFilter: VisibilityFilters.SHOW_ALL,
@@ -11,6 +11,19 @@ function todoApp(state = initialState, action){
     	   return Object.assign({}, state, {
     	   	visibilityFilter: action.filter
     	   })
+
+    	 case ADD_TODO :
+    	    return Object.assign(
+    	    	{}, state, {
+    	    		todos: [
+    	    			...state.todos,
+    	    			{
+    	    				text: action.text,
+    	    				completed: false
+    	    			}
+    	    		]
+    	    	})
+    	    
     	 default:
     	 	return state
     }
