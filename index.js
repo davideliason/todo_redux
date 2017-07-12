6578d3354a44e5049e540888dba05e0debf300c7
davideliason <davethemaker@gmail.com>
2017-07-12 14:45:19 -0700

    tidy up the code

--- index.js
+++ index.js
@@ -5,38 +5,34 @@ const initialState = {
 	todos: []
 }
 
-function todoApp(state = initialState, action){
-    switch(action){
-    	case SET_VISIBILITY_FILTER:
-    	   return Object.assign({}, state, {
-    	   	visibilityFilter: action.filter
-    	   })
-
-    	 case ADD_TODO :
-    	    return Object.assign(
-    	    	{}, state, {
-    	    		todos: [
-    	    			...state.todos,
-    	    			{
-    	    				text: action.text,
-    	    				completed: false
-    	    			}
-    	    		]
-    	    	})
-
-    	  case TOGGLE_TODO: 
-    	    return Object.assign({}, state,{
-    	    	todos: state.todos.map( (todo,index) => {
-    	    		if(index === action.index){
-    	    			return Object.assign({},state, todo, {
-    	    				completed: !todo.completed
-    	    			})
-    	    		}
-    	    		return todo
-    	    	})
-    	    })
-
-    	 default:
-    	 	return state
-    }
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      })
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      })
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            })
+          }
+          return todo
+        })
+      })
+    default:
+      return state
+  }
 }
